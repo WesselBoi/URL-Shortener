d402ded2acc44e2db4d13ab4202a68b8ba73e2eb
WesselBoi <mathur.saksham2005@gmail.com>
2025-05-07 21:50:12 +0530

    nun

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const mongoose = require("mongoose");
 const urlRoute = require("./routes/url");
 const connectToMongoDB = require("./connection");
 const path = require("path");
-const {restrictToLoggedinUserOnly , checkAuth} = require("./middlewares/auth");
-
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
 
 const URL = require("./models/url");
 const staticRoute = require("./routes/staticRouter");
@@ -14,28 +13,23 @@ const userRoute = require("./routes/user");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-const PORT = 8001; 
-const mongoUrl=process.env.MONGODB_URI
-
-
+const PORT = 8001;
+const mongoUrl = process.env.MONGODB_URI;
 
 connectToMongoDB(mongoUrl)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Error connecting to MongoDB", err));
 
-
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false  }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/url", restrictToLoggedinUserOnly , urlRoute);       // Restrict access to logged-in users only by using inline middleware
+app.use("/url", restrictToLoggedinUserOnly, urlRoute); // Restrict access to logged-in users only by using inline middleware
 
-app.use("/", checkAuth , staticRoute)
-
+app.use("/", checkAuth, staticRoute);
 
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
@@ -44,17 +38,16 @@ app.get("/url/:shortId", async (req, res) => {
       shortId,
     },
     {
-        $push:{visitHistory : {
-            timestamp : Date.now()
-        }
-        }
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
     }
   );
   res.redirect(entry.redirectUrl);
 });
 
-
 app.use("/user", userRoute);
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const URL = require('../models/url');
+const URL = require("../models/url");
 
-router.get("/", async (req,res) => {
-    if (!req.user) return res.redirect("/login")
-    const allUrls = await URL.find({ createdBy : req.user._id})
-    return res.render("home" ,{
-        urls : allUrls
-    })
-})
+router.get("/", async (req, res) => {
+  if (!req.user) return res.redirect("/login");
+  const allUrls = await URL.find({ createdBy: req.user._id });
+  return res.render("home", {
+    urls: allUrls,
+  });
+});
 
-router.get("/signup" , (req,res) => {
-    return res.render("signup")
-})
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
 
-router.get("/login" , (req,res) => {
-    return res.render("login")
-})
+router.get("/login", (req, res) => {
+  return res.render("login");
+});
 
 module.exports = router;
